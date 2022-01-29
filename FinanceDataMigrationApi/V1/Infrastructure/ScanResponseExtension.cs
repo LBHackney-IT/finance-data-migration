8407a7ed90b7f8d52579211eea7bc39cbf3432ab
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-01-29 22:38:27 +0330

    Trace exception

--- FinanceDataMigrationApi/V1/Infrastructure/ScanResponseExtension.cs
+++ FinanceDataMigrationApi/V1/Infrastructure/ScanResponseExtension.cs
@@ -15,11 +15,6 @@ namespace FinanceDataMigrationApi.V1.Infrastructure
             foreach (Dictionary<string, AttributeValue> item in response.Items)
             {
                 LoggingHandler.LogInfo($"{nameof(FinanceDataMigrationApi)}.{nameof(Handler)}.{nameof(ToTenureInformation)}, response.Items: {item["id"].S}");
-                LoggingHandler.LogInfo($"{nameof(FinanceDataMigrationApi)}.{nameof(Handler)}.{nameof(ToTenureInformation)}, response.Items: {item["tenuredAsset"].M}");
-                LoggingHandler.LogInfo($"{nameof(FinanceDataMigrationApi)}.{nameof(Handler)}.{nameof(ToTenureInformation)}, response.Items: {item["tenureType"].M}");
-                LoggingHandler.LogInfo($"{nameof(FinanceDataMigrationApi)}.{nameof(Handler)}.{nameof(ToTenureInformation)}, response.Items: {item["terminated"].M}");
-                LoggingHandler.LogInfo($"{nameof(FinanceDataMigrationApi)}.{nameof(Handler)}.{nameof(ToTenureInformation)}, response.Items: {item["paymentReference"].S}");
-                LoggingHandler.LogInfo($"{nameof(FinanceDataMigrationApi)}.{nameof(Handler)}.{nameof(ToTenureInformation)}, response.Items: {item["householdMembers"].M}");
 
                 if (!item.ContainsKey("id"))
                     throw new Exception(response.ToString());
