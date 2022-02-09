b137b1bd18d36638f2e3f914bda21b07adf347de
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-02-09 14:19:05 +0330

    format fixing

--- FinanceDataMigrationApi/V1/UseCase/Accounts/LoadAccountsUseCase.cs
+++ FinanceDataMigrationApi/V1/UseCase/Accounts/LoadAccountsUseCase.cs
@@ -35,7 +35,7 @@ namespace FinanceDataMigrationApi.V1.UseCase.Accounts
                     List<Task> tasks = new List<Task>();
                     for (int i = 0; i <= extractedList.Count / _batchSize; i++)
                     {
-                        var data = extractedList.Where(s=>s.TargetId!=null)
+                        var data = extractedList.Where(s => s.TargetId != null)
                             .OrderBy(p => p.Id).Skip(i * _batchSize).Take(_batchSize).ToList();
                         if (data.Any())
                         {
