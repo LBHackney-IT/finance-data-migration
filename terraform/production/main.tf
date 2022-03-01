aa65c9312a241a41d52ab106db4f91b00828c0ee
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-03-01 21:38:00 +0330

    S3 bucket removed in terraform

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -25,12 +25,3 @@ data "aws_region" "current" {}
 locals {
     parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
-
-terraform {
-    backend "s3" {
-        bucket  = "terraform-state-housing-development"
-        encrypt = true
-        region  = "eu-west-2"
-        key     = "services/finance-data-migration-api/state"
-    }
-}
